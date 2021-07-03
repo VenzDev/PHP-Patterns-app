6237c79360ea5d6d471e67704fd5cf66f18a8cc1
VenzQ <kamil.bechta1@gmail.com>
2021-07-03 17:00:52 +0200

    check headers

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 use App\Controllers\Controller;
 use App\Kernel;
 use Dotenv\Dotenv;
